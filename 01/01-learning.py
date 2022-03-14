31d894c82b5dfec223470ac3d199cc53fe43c1d3
Yeison <enmanuelquiterios@gmail.com>
2022-03-13 23:57:58 -0400

    Module Operator

--- 01/01-learning.py
+++ 01/01-learning.py
@@ -61,6 +61,18 @@ quilt_length = 8
 # Let's see how large it is now
 print(quilt_width * quilt_length)
 
+# Moduke
+
+# Prints 4 because 29 / 5 is 5 with a remainder of 4
+print(29 % 5)
+ 
+# Prints 2 because 32 / 3 is 10 with a remainder of 2
+print(32 % 3)
+ 
+# Modulo by 2 returns 0 for even numbers and 1 for odd numbers
+# Prints 0
+print(44 % 2)
+
 
 
 
