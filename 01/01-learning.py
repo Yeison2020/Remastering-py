7fd75e758dcb113fbdc2ed796386c078d0e831a3
Yeison <enmanuelquiterios@gmail.com>
2022-03-13 23:52:18 -0400

    Adding quilt_length

--- 01/01-learning.py
+++ 01/01-learning.py
@@ -48,6 +48,21 @@ print(coffe_price * number_of_coffees)
 
 
 
+# First we create the variables
+quilt_width = 8
+quilt_length = 12
+
+# Then we print the size
+print(quilt_width * quilt_length)
+
+# Whoops! That's a little too large
+quilt_length = 8
+
+# Let's see how large it is now
+print(quilt_width * quilt_length)
+
+
+
 
 
 
