da2242c0721b390a6f94d3be593765f99295f90e
Yeison <enmanuelquiterios@gmail.com>
2022-03-13 23:38:13 -0400

    Working with Numbers

--- 01/01-learning.py
+++ 01/01-learning.py
@@ -7,12 +7,23 @@ persnickety_count = 0
 # This code will calculate the likelihood that it will rain tomorrow
 #complicated_rain_calculation_for_tomorrow
 
-print('Hello World here I\'m learning python again')
+# print('Hello World here I\'m learning python again')
 
 # desclaring variables in Python
 
 
 message_string = "Hello World here is the important message for all"
+print(message_string)
+
+# Here I can reassign the variable to another value
+name_user = "Enmanuel Quiterios"
+# Here the variable changed It's value and was printed with the new value.
+message_string = "Hasta la vista baby"
 
 
 print(message_string)
+
+
+# Numbers in Python 
+
+
