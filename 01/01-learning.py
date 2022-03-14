0b8f41e2b4a1b851033716b77d4c86351fa8d934
Yeison <enmanuelquiterios@gmail.com>
2022-03-13 23:25:27 -0400

    Adding keywords

--- 01/01-learning.py
+++ 01/01-learning.py
@@ -1,2 +1,10 @@
 #First steps how to write and comment we use the Symbol # to comment any python line of code.
 
+
+#This variable will be sued tp coynt the numbers of tweets the word
+persnickety_count = 0
+
+# This code will calculate the likelihood that it will rain tomorrow
+#complicated_rain_calculation_for_tomorrow
+
+print('Hello World here I\'m learning python again')
