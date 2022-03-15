667b62fb4e58f74aa4914d6c043607a733cf351d
Yeison <enmanuelquiterios@gmail.com>
2022-03-14 22:29:04 -0400

    Working with not

--- initial /boolean.py
+++ initial /boolean.py
@@ -107,6 +107,34 @@ if credits >= 120 or gpa >= 2.0:
 #Boolean Operators: not
 
 
+# Reverse the value 
+
+not True == False
+not False == True
+
+
+statement_one = not (4 + 5 <= 9)
+
+statement_two =  not (8 * 2) != 20 - 4
+
+credits = 120
+gpa = 1.8
+
+
+
+if not credits >= 120:
+  print("You do not have enough credits to graduate.") 
+
+
+
+if not gpa >= 2.0:
+  print("Your GPA is not high enough to graduate.")
+
+
+if( not credits >=  120) and (not gpa >= 2.0):
+  print("You do not meet either requirement to graduate!")
+
+
 
 
 
