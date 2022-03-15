d1f33d77d36de48f58b19358eb7d10b1c66a6aaa
Yeison <enmanuelquiterios@gmail.com>
2022-03-15 00:28:21 -0400

    Concatinations of List

---
+++ built-dataStructures/built_Data_Structures.py
@@ -0,0 +1,44 @@
+
+
+# What is a List?
+
+heights = [61, 70, 67, 64]
+
+#broken_heights = [65 71 59 62]
+
+heights = [61, 70, 67, 64, 65]
+
+broken_heights = [65, 71, 59, 62]
+
+ints_and_strings = [1, 2, 3, "four", "five", "ints_and_strings"]
+
+
+sam_height_and_testscore = ["Sam" ,67, 85.5 , True]
+
+
+#Growing a List: Plus (+)
+
+
+items_sold = ["cake", "cookie", "bread"]
+
+
+items_sold_new = items_sold + ["biscuit", "tart"]
+print(items_sold_new)
+
+
+orders = ["daisy", "buttercup", "snapdragon", "gardenia", "lily"]
+
+# Create new orders here:
+
+
+
+broken_prices = [5, 3, 4, 5, 4] + [4]
+
+
+
+new_orders = ["lilac", "iris"]
+
+orders_combined = orders + new_orders
+
+
+
