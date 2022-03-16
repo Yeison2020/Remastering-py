9ca9837bea6be8c4503f5c4a9946bb6776e05b20
Yeison <enmanuelquiterios@gmail.com>
2022-03-15 20:42:54 -0400

    Removing from a list

--- built-dataStructures/built_Data_Structures.py
+++ built-dataStructures/built_Data_Structures.py
@@ -181,3 +181,30 @@ full_gradebook = gradebook + last_semester_gradebook
 
 
 print(full_gradebook)
+
+
+# Adding by Index: Insert
+
+front_display_list = ["Mango", "Filet Mignon", "Chocolate Milk"]
+print(front_display_list)
+
+# Your code below: 
+
+store_line = ["Karla", "Maxium", "Martim", "Isabella"]
+
+
+data_science_topics = ["Machine Learning", "SQL", "Pandas", "Algorithms", "Statistics", "Python 3"]
+print(data_science_topics)
+
+# Your code below: 
+
+data_science_topics.pop()
+
+print(data_science_topics)
+
+data_science_topics.pop(3)
+
+print(data_science_topics)
+
+
+
