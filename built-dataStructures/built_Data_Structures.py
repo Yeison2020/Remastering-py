6baae1e574da90b30e344109df51c07aa1e49eef
Yeison <enmanuelquiterios@gmail.com>
2022-03-15 20:15:33 -0400

    Updating 2D list elements

--- built-dataStructures/built_Data_Structures.py
+++ built-dataStructures/built_Data_Structures.py
@@ -96,3 +96,42 @@ print(incoming_class)
 incoming_class[-3][-3] = "Ken"
 
 print(incoming_class)
+
+
+# Your code below: 
+
+first_names = ['Ainsley',
+'Ben',
+'Chani',
+'Depak']
+
+preferred_size = ["Small", "Large", "Medium"]
+
+preferred_size.append("Medium")
+
+print(preferred_size)
+
+
+
+customer_data = [["Ainsley"	,"Small"	,True],
+["Ben",	"Large",	False],
+["Chani","Medium",	True],
+["Depak",	"Medium",	False]]
+
+
+print(customer_data)
+
+
+customer_data[-2][-1] = False
+
+print(customer_data)
+
+customer_data_final  = customer_data + [["Amit", "Large", True], ["Karim", "X-Large", False]]
+
+
+customer_data[1].remove(False)
+
+print(customer_data)
+
+
+print(customer_data_final)
